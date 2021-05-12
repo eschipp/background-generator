33ca33c80cbf4c9775685fc2dd8cb3ff25fcbeb5
Erin <erin.schippers@gmail.com>
2021-05-11 21:56:36 -0400

    new files

---
+++ script.js
@@ -0,0 +1,22 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let button = document.getElementById("button");
+
+css.textContent = "linear-gradient(to right, #ff0000 , #ffff00);";
+
+function setGradient() {
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	setGradient();
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+button.addEventListener("click", getRandomColor);
